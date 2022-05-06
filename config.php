918110b3617460f368572c4b1dce0f2be7851126
gspgaby <gspgaby@gmail.com>
2022-05-05 23:45:07 -0300

    mundança config para deploy

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+$nome_sistema = 'SME - GERENCIADOR DE ESCOLAS';
+$nome_adm = 'Gabriella dos Santos Pereira';
+$email_adm = 'gspgaby@gmail.com';
+
+$servidor = 'us-cdbr-east-05.cleardb.net';
+$usuario = 'b3bc11dbd0a597';
+$senha = '63964358';
+$banco = 'heroku_93153fe78153306';
+?>
